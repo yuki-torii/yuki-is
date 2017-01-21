82b2c70c0efd804f3e165ad652524837cd127859
limichange <limichange@hotmail.com>
2017-01-21 11:34:22 +0800

    ✨  - Add new features.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const is = {
+  string (value) {
+    return toString.call(value) === '[object String]'
+  },
+  object (value) {
+    return Object(value) === value
+  },
+  null (value) {
+    return value === null
+  }
+}
+
+module.exports = is
