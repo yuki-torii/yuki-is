3403586f708c6d86aa2e1a308318a08dc3c7a52e
limichange <limichange@hotmail.com>
2017-01-29 19:11:58 +0800

    📦  - Updating compiled files or packages.

--- dist/yuki-is.js
+++ dist/yuki-is.js
@@ -16,8 +16,68 @@ var type = {
   }
 };
 
+var navigator = window && window.navigator;
+var appVersion = (navigator && navigator.appVersion || '').toLowerCase();
+var userAgent$1 = (navigator && navigator.userAgent || '').toLowerCase();
+var vendor = (navigator && navigator.vendor || '').toLowerCase();
+
+var comparator = {
+  '<': function (a, b) { return a < b },
+  '<=': function (a, b) { return a <= b },
+  '>': function (a, b) { return a > b },
+  '>=': function (a, b) { return a >= b }
+};
+
+var utils = {
+  navigator: navigator,
+  appVersion: appVersion,
+  userAgent: userAgent$1,
+  vendor: vendor,
+  compareVersion: function compareVersion (version, range) {
+    var string = (range + '');
+    var n = +(string.match(/\d+/) || NaN);
+    var op = string.match(/^[<>]=?|/)[0];
+    return comparator[op] ? comparator[op](version, n) : (version === n)
+  }
+};
+
+var ref = utils;
+var userAgent = ref.userAgent;
+var compareVersion = ref.compareVersion;
+
+var environment = {
+  android: function android () {
+    return /android/.test(userAgent)
+  },
+  androidPhone: function androidPhone () {
+    return this.android() && this.mobile()
+  },
+  androidTablet: function androidTablet () {
+    return this.android() && !this.mobile()
+  },
+  mobile: function mobile () {
+    return /mobile/.test(userAgent)
+  },
+  ios: function ios () {
+    return this.iphone() || this.ipad() || this.ipod()
+  },
+  iphone: function iphone (range) {
+    var match = this.ipad() ? null : userAgent.match(/iphone(?:.+?os (\d+))?/);
+    return match !== null && compareVersion(match[1] || 1, range)
+  },
+  ipad: function ipad (range) {
+    var match = userAgent.match(/ipad.+?os (\d+)/);
+    return match !== null && compareVersion(match[1], range)
+  },
+  ipod: function ipod (range) {
+    var match = userAgent.match(/ipod.+?os (\d+)/);
+    return match !== null && compareVersion(match[1], range)
+  }
+};
+
 var index = {
-  type: type
+  type: type,
+  environment: environment
 };
 
 return index;
