84f6a6d7d41870dd5e6788ed26bdb292775391b6
limichange <limichange@hotmail.com>
2017-02-09 10:54:34 +0800

    📦  - Updating compiled files or packages.

--- dist/yuki-is.es5.js
+++ dist/yuki-is.es5.js
@@ -1,18 +1,6 @@
-var type = {
-  string: function string (value) {
-    return toString.call(value) === '[object String]'
-  },
-  object: function object (value) {
-    return Object(value) === value
-  },
-  null: function null$1 (value) {
-    return value === null
-  }
-};
-
 var navigator = window && window.navigator;
 var appVersion = (navigator && navigator.appVersion || '').toLowerCase();
-var userAgent$1 = (navigator && navigator.userAgent || '').toLowerCase();
+var userAgent = (navigator && navigator.userAgent || '').toLowerCase();
 var vendor = (navigator && navigator.vendor || '').toLowerCase();
 
 var comparator = {
@@ -22,10 +10,11 @@ var comparator = {
   '>=': function (a, b) { return a >= b }
 };
 
-var utils = {
+var utils$1 = {
+  toString: Object.prototype.toString,
   navigator: navigator,
   appVersion: appVersion,
-  userAgent: userAgent$1,
+  userAgent: userAgent,
   vendor: vendor,
   compareVersion: function compareVersion (version, range) {
     var string = (range + '');
@@ -35,13 +24,30 @@ var utils = {
   }
 };
 
-var ref = utils;
-var userAgent = ref.userAgent;
+var utils = utils$1;
+
+var type = {
+  string: function string (value) {
+    return utils.toString.call(value) === '[object String]'
+  },
+  object: function object (value) {
+    return Object(value) === value
+  },
+  null: function null$1 (value) {
+    return value === null
+  },
+  array: function (value) {
+    return utils.toString.call(value) === '[object Array]'
+  }
+};
+
+var ref = utils$1;
+var userAgent$1 = ref.userAgent;
 var compareVersion = ref.compareVersion;
 
 var environment = {
   android: function android () {
-    return /android/.test(userAgent)
+    return /android/.test(userAgent$1)
   },
   androidPhone: function androidPhone () {
     return this.android() && this.mobile()
@@ -50,21 +56,21 @@ var environment = {
     return this.android() && !this.mobile()
   },
   mobile: function mobile () {
-    return /mobile/.test(userAgent)
+    return /mobile/.test(userAgent$1)
   },
   ios: function ios () {
     return this.iphone() || this.ipad() || this.ipod()
   },
   iphone: function iphone (range) {
-    var match = this.ipad() ? null : userAgent.match(/iphone(?:.+?os (\d+))?/);
+    var match = this.ipad() ? null : userAgent$1.match(/iphone(?:.+?os (\d+))?/);
     return match !== null && compareVersion(match[1] || 1, range)
   },
   ipad: function ipad (range) {
-    var match = userAgent.match(/ipad.+?os (\d+)/);
+    var match = userAgent$1.match(/ipad.+?os (\d+)/);
     return match !== null && compareVersion(match[1], range)
   },
   ipod: function ipod (range) {
-    var match = userAgent.match(/ipod.+?os (\d+)/);
+    var match = userAgent$1.match(/ipod.+?os (\d+)/);
     return match !== null && compareVersion(match[1], range)
   }
 };
